785e7bf39ff7f7ef87ac3340fc9c363d079312a6
miguelmedrado <86388812+miguelmedrado@users.noreply.github.com>
2022-06-22 11:22:23 -0300

    Update Subtracao.java

--- Subtracao.java
+++ Subtracao.java
@@ -4,27 +4,21 @@ import java.io.OutputStream;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-public class subtracao implements HttpHandler{
+public class Subtracao implements HttpHandler{
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         try {
             String[] partes = exchange.getRequestURI().getPath().split("/");
-            if (partes[2].equals("subtracao")) {
-                byte[] resposta = "Tu que eh".getBytes();
+            float n1 = Float.parseFloat(partes[2]);
+            float n2 = Float.parseFloat(partes[3]);
+            float sub = n1-n2;
+                byte[] resposta = Float.toString(sub).getBytes();  
                 exchange.sendResponseHeaders(200, resposta.length);
                 exchange.getResponseBody().write(resposta);
-                return;
-            } 
-            
-            byte[] resposta = "A Bianca que eh".getBytes();
-            exchange.sendResponseHeaders(200, resposta.length);
-            exchange.getResponseBody().write(resposta);
-        } catch (Exception e) {
-            e.printStackTrace();
+                return; 
+    
         } finally {
             exchange.close();
         }
     }
-
-    
 }
