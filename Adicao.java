797f73793cbe2c36e7822f59b6a60cc90463cab7
JhonathanSC <jsc6@aluno.ifal.edu.br>
2022-06-22 14:18:39 +0000

    Endereçando comentário

--- Adicao.java
+++ Adicao.java
@@ -12,8 +12,8 @@ public class Adicao implements HttpHandler{
             String[] partes = exchange.getRequestURI().getPath().split("/");
             float n1 = Float.parseFloat(partes[2]);
             float n2 = Float.parseFloat(partes[3]);
-            float add = n1+n2;
-                byte[] resposta = Float.toString(add).getBytes();  
+            float adicao = n1+n2;
+                byte[] resposta = Float.toString(adicao).getBytes();  
                 exchange.sendResponseHeaders(200, resposta.length);
                 exchange.getResponseBody().write(resposta);
                 return; 
