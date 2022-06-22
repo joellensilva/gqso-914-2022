c9cfa2e92fbb898473b5d25b6e6107132a6222fe
miguelmedrado <86388812+miguelmedrado@users.noreply.github.com>
2022-06-22 11:40:41 -0300

    Endereçando comentário

--- Subtracao.java
+++ Subtracao.java
@@ -11,8 +11,8 @@ public class Subtracao implements HttpHandler{
             String[] partes = exchange.getRequestURI().getPath().split("/");
             float n1 = Float.parseFloat(partes[2]);
             float n2 = Float.parseFloat(partes[3]);
-            float sub = n1-n2;
-                byte[] resposta = Float.toString(sub).getBytes();  
+            float subtracao = n1-n2;
+                byte[] resposta = Float.toString(subtracao).getBytes();  
                 exchange.sendResponseHeaders(200, resposta.length);
                 exchange.getResponseBody().write(resposta);
                 return; 
