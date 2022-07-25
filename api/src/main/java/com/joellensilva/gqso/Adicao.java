afb9d5cb4d6489dc06a4801ba38e994324b9fc5d
JhonathanSC <jsc6@aluno.ifal.edu.br>
2022-07-25 09:14:45 -0300

    Adicao

--- api/src/main/java/com/joellensilva/gqso/Adicao.java
+++ api/src/main/java/com/joellensilva/gqso/Adicao.java
@@ -1,4 +1,3 @@
-
 package com.joellensilva.gqso;
 
 import io.jooby.annotations.GET;
@@ -6,15 +5,15 @@ import io.jooby.annotations.Path;
 import io.jooby.annotations.PathParam;
 import io.jooby.exception.BadRequestException;
 
-@Path("/multiplicacao/{n1}/{n2}")
-public class Multiplicacao {
+@Path("/adicao/{n1}/{n2}")
+public class Adicao {
 
     @GET
-    public double calculaMult(@PathParam("n1") String n1, @PathParam("n2") String n2) {
+    public float calculaAdic(@PathParam("n1") String n1, @PathParam("n2") String n2) {
         float num1 = Float.parseFloat(n1);
         float num2 = Float.parseFloat(n2);
         try {
-            return num1 * num2;
+            return num1 + num2;
         } catch (NumberFormatException nfe) {
             throw new BadRequestException("Parâmetro inválido: " + n1 + " e " + n2);
         }
