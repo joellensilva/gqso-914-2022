e65c5a5cb561a7d40409b88b30fe18f35d84cbdf
JhonathanSC <jsc6@aluno.ifal.edu.br>
2022-08-31 11:21:43 -0300

    int adicao

--- api/src/test/java/com/joellensilva/gqso/IntegrationTest.java
+++ api/src/test/java/com/joellensilva/gqso/IntegrationTest.java
@@ -17,14 +17,24 @@ public class IntegrationTest {
   static OkHttpClient client = new OkHttpClient();
 
   @Test
-  public void shouldSayHi(int serverPort) throws IOException {
+  public void adicao(int serverPort) throws IOException {
     Request req = new Request.Builder()
-        .url("http://localhost:" + serverPort)
+        .url("http://localhost:" + serverPort + "/adicao/5/6")
         .build();
 
     try (Response rsp = client.newCall(req).execute()) {
-      assertEquals("Bem-vindo à nossa API!", rsp.body().string());
+      assertEquals("11.0", rsp.body().string());
       assertEquals(StatusCode.OK.value(), rsp.code());
+    }
+  }
+  @Test
+  public void Erro_Adicao(int serverPort) throws IOException {
+    Request req = new Request.Builder()
+        .url("http://localhost:" + serverPort + "/adicao/j/c")
+        .build();
+
+    try (Response rsp = client.newCall(req).execute()) {
+      assertEquals(StatusCode.BAD_REQUEST.value(), rsp.code());
     }
   }
 }
